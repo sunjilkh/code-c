e71b03dd73e6cad6860f5ecdf7ecd312ae835a66
sunjilkh <110080955+sunjilkh@users.noreply.github.com>
2022-11-08 23:43:52 +0600

    Update 1749A.c

--- 1749A.c
+++ 1749A.c
@@ -1,3 +1,24 @@
+// old version
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int n,m;
+        scanf("%d %d",&n,&m);
+        int x = m;
+        while(x--){
+            int p,q;
+            scanf("%d%d",&p,&q);
+        }
+        if(m == n-1) printf("YES\n");
+        else if( m == 1 && n>1) printf("YES\n");
+        else printf("NO\n");
+    }
+}
+
+// New
 #include<stdio.h>
 int main()
 {
